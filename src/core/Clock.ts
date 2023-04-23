ff7df8a29af7e0641d5784e9260f38eb4b6669f6
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2023-04-23 18:04:14 +0200

    Refactor: create TestableClock to assure a fixed date in tests

--- src/core/Clock.ts
+++ src/core/Clock.ts
@@ -1,6 +1,9 @@
 export class Clock {
     getDateAsString(){
-        const date = new Date();
-        return date.toLocaleDateString('es-ES', {year: 'numeric', month: '2-digit', day: '2-digit'});
+        return this.today().toLocaleDateString('es-ES', {year: 'numeric', month: '2-digit', day: '2-digit'});
+    }
+
+    protected today(){
+        return new Date();
     }
 }
