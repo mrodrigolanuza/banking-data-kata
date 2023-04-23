a20bc4a77e9aac52cb8d3beedb5d66b8156fccc0
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2023-04-23 19:22:09 +0200

    Red: prints a statament account including several transactions

--- src/tests/unit/StatementPrinter.test.ts
+++ src/tests/unit/StatementPrinter.test.ts
@@ -3,11 +3,11 @@ import { StatementPrinter } from "../../core/StatementPrinter";
 import { Transaction } from "../../core/Transaction";
 
 describe('The Statement Printer', () => {
-    it('prints correctly the header line', () =>{
-      const console = new Console();
-      const printer = new StatementPrinter(console);
-      const consoleSpy = jest.spyOn(console, 'log');
+    const console = new Console();
+    const printer = new StatementPrinter(console);
+    const consoleSpy = jest.spyOn(console, 'log');
 
+    it('prints correctly the header line', () =>{
       printer.print([new Transaction('', 0)]);
 
       expect(consoleSpy).toHaveBeenCalledWith('Date | Amount | Balance');
@@ -16,16 +16,25 @@ describe('The Statement Printer', () => {
         const date = '24/04/2023'
         const amount = 100;
         const balance = 100;
-        const console = new Console();
-        const printer = new StatementPrinter(console);
         const transactions = [new Transaction(date, amount)];
-        const consoleSpy = jest.spyOn(console, 'log');
         
         printer.print(transactions);
         
         expect(consoleSpy).toHaveBeenCalledWith('Date | Amount | Balance');
         expect(consoleSpy).toHaveBeenCalledWith(`${date} | ${amount.toFixed(2)} | ${balance.toFixed(2)}`);
-
-        expect(true).toBe(true);
       });
+      it('prints a statament account including several transactions', () =>{
+        const transactions = [
+            new Transaction('25/04/2023', 1000), 
+            new Transaction('26/04/2023', -500), 
+            new Transaction('27/04/2023', 2000)
+        ];
+        
+        printer.print(transactions);
+        
+        expect(consoleSpy).toHaveBeenCalledWith('Date | Amount | Balance');
+        expect(consoleSpy).toHaveBeenCalledWith('25/04/2023 | 1000.00 | 1000.00');
+        expect(consoleSpy).toHaveBeenCalledWith('25/04/2023 | -500.00 |  500.00');
+        expect(consoleSpy).toHaveBeenCalledWith('25/04/2023 | 2000.00 | 2500.00');
+   });
 });
