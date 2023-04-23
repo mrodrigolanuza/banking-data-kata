1d96b3556f1642049691e906309e52b24c72b5d0
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2023-04-23 19:53:35 +0200

    Green: prints an account statement including the transaction made throughout the console

--- src/tests/acceptance/PrintStatement.test.ts
+++ src/tests/acceptance/PrintStatement.test.ts
@@ -10,19 +10,26 @@ describe('Print Statement', () => {
     const console = new Console();
     const statementPrinter = new StatementPrinter(console);
     const consoleSpy = jest.spyOn(console, 'log');
-    const repository = new TransactionRepository(new Clock());
+    const clock = new Clock();
+    //Stub que permite devolver valores predeterminadas en un número de llamadas consecutivas
+    clock.getDateAsString = jest
+        .fn()
+        .mockReturnValueOnce('20/04/2023')
+        .mockReturnValueOnce('21/04/2023')
+        .mockReturnValueOnce('22/04/2023');
+    const repository = new TransactionRepository(clock);
     const account = new Account(repository, statementPrinter);
     
     it('prints an account statement including the transaction made throughout the console', () =>{
-        account.deposit(1000.00);
-        account.withdraw(500.00);
         account.deposit(2000.00);
+        account.withdraw(500.00);
+        account.deposit(1000.00);
         
         account.printStatement();
         
         expect(consoleSpy).toHaveBeenCalledWith('Date | Amount | Balance');
-        expect(consoleSpy).toHaveBeenCalledWith('23/04/2023 | 2000.00 | 2500.00');
-        expect(consoleSpy).toHaveBeenCalledWith('22/04/2023 | -500.00 | 500.00');
-        expect(consoleSpy).toHaveBeenCalledWith('22/04/2023 | 1000.00 | 1000.00');
+        expect(consoleSpy).toHaveBeenCalledWith('20/04/2023 | 2000.00 | 2000.00');
+        expect(consoleSpy).toHaveBeenCalledWith('21/04/2023 | -500.00 | 1500.00');
+        expect(consoleSpy).toHaveBeenCalledWith('22/04/2023 | 1000.00 | 2500.00');
     });
 });
